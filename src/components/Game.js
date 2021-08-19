36658ef98df39340432f249de677a0543dc38e75
Sanjeet Sidhu <Sanjeetu3@gmail.com>
2021-08-19 01:03:28 -0700

    added working for now WPM calculations

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,16 +11,63 @@ const Game = () => {
 
   const currentQuote = useRef(quote?.quoteText);
 
+  const gameTimeout = useRef();
+  const gameDuration = useRef(0);
+
+  function gameCountdown() {
+    const now = Date.now();
+    const then = now;
+    gameTimeout.current = setInterval(() => {
+      gameDuration.current = Math.round((Date.now() - then) / 1000);
+      calcCPM(
+        gameDuration.current,
+        currentQuote.current.length,
+        completedWords.join("").length
+      );
+    }, 1000);
+  }
+
   useEffect(() => {
     console.log("new game");
     currentQuote.current = quote.quoteText;
     setCompletedWords([]);
     if (currentWord !== "") {
       //if user plays a game, then plays another this resets the currword to be typed to '' then on next rerender it knows its been reset and next useffect picks up the correct curr word for the game to be played
-      console.log("reset curr word");
+      console.log("reset curr word & timer");
+      clearInterval(gameTimeout.current);
       setCurrentWord("");
     }
+    if (typeof quote === "object") {
+      console.log("start timer");
+      gameCountdown();
+    }
+    () => clearInterval(gameTimeout.current);
   }, [quote]);
+
+  useEffect(() => {
+    if (
+      !currentWord &&
+      completedWords.join("").trim() === currentQuote.current?.trim()
+    ) {
+      console.log("finished");
+      alert("congrats it just took you " + gameDuration.current + " seconds.");
+      clearInterval(gameTimeout.current);
+    }
+  }, [currentWord]);
+
+  function calcCPM(seconds, quoteLen, completedLen) {
+    let writtenWordsContainer = document.querySelector(".quoteGoesHere");
+    //the timeout is set on render, therfore the completed words array is forever at [''] I need to make my own setInterval solution.
+    const completedLenTempSolution =
+      writtenWordsContainer.childNodes[0].textContent.concat(
+        writtenWordsContainer.childNodes[1].textContent
+      ).length;
+    const CPMtoWPM = (
+      (completedLenTempSolution / 5) *
+      (60 / seconds)
+    ).toFixed();
+    console.log(CPMtoWPM);
+  }
 
   function saveUserInput(input) {
     setUserInput(input);
